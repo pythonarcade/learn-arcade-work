9f11799ce46e849a35599848d6ab1635297a0c34
jdh2019 <hulley.johndean@gmail.com>
2021-02-16 14:08:28 +0000

    Lab01 Completed

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,8 @@
+print("Lab 01 is all about the 'Print' statement.")
+
+print("""Can I use this print statements in four different 
+ways to show how print can be utilized in different use cases. 
+""")
+
+print("Of \ncourse \nI \ncan. \nI \nam \nthe \nbest \nat \nthis.")
+print("\tI can even do fancy \n\tindentation.")
