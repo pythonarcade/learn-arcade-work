195dfc0d812e84f3c8ee862af5fd9e8f52d5d935
Aschoolcoder <89813262+Aschoolcoder@users.noreply.github.com>
2021-09-14 09:41:22 -0700

    Updated drawing samples, figured out how to use rgb to make custom color.

--- Testing/drawing_samples.py
+++ Testing/drawing_samples.py
@@ -13,7 +13,7 @@ import arcade
 arcade.open_window(600, 600, "Drawing Example")
 
 # Set the background color
-arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+arcade.set_background_color((31, 218, 255))
 
 # Get ready to draw
 arcade.start_render()
@@ -26,19 +26,29 @@ arcade.start_render()
 # Center of 100, 320
 # Width of 20
 # Height of 60
-arcade.draw_rectangle_filled(100, 320, 20, 90, arcade.csscolor.SIENNA)
+arcade.draw_rectangle_filled(100, 320, 20, 90, (163, 99, 11))
 
 # Tree top
-arcade.draw_circle_filled(100, 350, 30, arcade.csscolor.DARK_GREEN)
+arcade.draw_circle_filled(100, 350, 30, (245, 138, 61))
 
-arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
+arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, (168, 131, 29))
 
 # Another tree, with a trunk and ellipse for top
-arcade.draw_rectangle_filled(200, 320, 20, 60, arcade.csscolor.SIENNA)
-arcade.draw_ellipse_filled(200, 370, 60, 80, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(200, 320, 20, 40, arcade.csscolor.SIENNA)
+arcade.draw_ellipse_filled(200, 370, 60, 80, (156, 55, 25))
 # Finish drawing
+# Another tree, with a trunk and arc for top
+# Arc is centered at (300, 340) with a width of 60 and height of 100.
+# The starting angle is 0, and ending angle is 180.
+arcade.draw_rectangle_filled(300, 320, 20, 40, arcade.csscolor.SIENNA)
+arcade.draw_arc_filled(300, 320, 40, 200, (237, 201, 43), 0, 180)
 
+
+# Another tree, with a trunk and triangle for top
+# Triangle is made of these three points:
+# (400, 400), (370, 320), (430, 320)
+arcade.draw_rectangle_filled(400, 320, 20, 40, arcade.csscolor.SIENNA)
+arcade.draw_triangle_filled(400, 500, 370, 320, 430, 320, arcade.csscolor.DARK_GREEN)
 arcade.finish_render()
-
 # Keep the window up until someone closes it.
 arcade.run()
