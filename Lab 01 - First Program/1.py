7f0f80c25756eef1c5ba2b3da054f86d77e79149
rosik <rosik911@gmail.com>
2022-04-05 18:18:03 +0300

    first test paint with arcade

--- Lab 01 - First Program/1.py
+++ Lab 01 - First Program/1.py
@@ -1,1 +1,13 @@
-print ("first merge request")
+"""
+This is a sample program to show how to draw using the Python programming
+language and the Arcade library.
+"""
+
+# Import the "arcade" library
+import arcade
+
+# Open up a window.
+# From the "arcade" library, use a function called "open_window"
+# Set the dimensions (width and height)
+# Set the window title to "Drawing Example"
+arcade.open_window(600, 600, "Drawing Example")
