16f26bc5d3d30049c1d81ab5380271903abc7bb8
RosikOsadchy <you@example.com>
2022-11-17 16:50:44 +0200

    456321789

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -1,3 +1,4 @@
+import random
 def main():
     print("""                 Welcome to Camel!
     You have stolen a camel to make your way across the great Mobi desert.
@@ -7,29 +8,79 @@ def main():
     done = False
     way_miles = 0
     camael_tired = 0
-    water_balbance = 0
-    water_botles = 3
+    water_balance = 0
+    water_botles = 4
     tusems_way = -20
 
     while not done:
       
         choise = (input("""
-        A. Drink from your canteen.
-        B. Ahead moderate speed.
-        C. Ahead full speed.
-        D. Stop for the night.
-        E. Status check.
-        Q. Quit.
+        A. Выпить воды с запасов.
+        B. Двигаться вперед.
+        C. Двигаться вперед на полной скорости.
+        D. Остановиться на ночь.
+        E. Проверить статус.
+        Q. Выйти.
         
-        What is your choice? """))
+        Сделайте свой выбор? """))
         if choise.upper() == "Q":
             done = True
-            print("you quit")
+            print("Вы вышли")
         elif choise.upper() == "E":
-            print("Miles traveled:", way_miles)
-            print("Drinks in canteen:", water_botles)
-            print("The natives are  " + str(way_miles - tusems_way) + "  miles behind you.")
-            
+            print("Пройдено миль:", way_miles)
+            print("Бутылок в запасах:", water_botles)
+            print("Туземцы на  " + str(way_miles - tusems_way) + "  миль позади тебя.")
+        elif choise.upper() == "D":
+            print("Верблюд отдохнул, усталость сброшена")
+            camael_tired =  0
+            tusems_way =+ random.randrange(7, 15)
+        elif choise.upper() == "C":
+            water_balance += 1
+            way_miles += random.randrange(10, 21)
+            camael_tired += random.randrange(1, 4)
+            tusems_way += random.randrange(7, 15)
+            print("Вы прошли - " + str(way_miles) + " миль")
+        elif choise.upper() == "B":
+            water_balance += 1
+            way_miles += random.randrange(5, 13)
+            camael_tired += 1
+            tusems_way += random.randrange(7, 15)
+            print("Вы прошли - " + str(way_miles) + " миль")
+        elif choise.upper() == "A":
+            if water_botles > 0:
+                water_balance = 0
+                water_botles -= 1
+            else:
+                print("Вода закончилась")
+        while water_balance in range(4, 7):
+            print("Вы хотите пить")
+            break
+        while water_balance > 6:
+            print("Вы умерли от жажды")
+            done = True
+            break
+        while camael_tired in range(5, 9):
+            print("Ваш верблюд устал, необходимо остановиться на ночь для отдыха")
+            break
+        while camael_tired > 8:
+            print("Ваш верблюд умер, вы проиграли")
+            done = True
+            break
+        while tusems_way == way_miles:
+            print("Туземцы вас поймали, вы проиграли")
+            done = True
+            break
+        while way_miles - tusems_way < 15:
+            print("Туземцы приближаются!")
+            break
+        while way_miles > 200:
+            print("Вы прошли пустыню, и выиграли")
+            done = True
+            break
+
+
+        
+                    
             
 
     
