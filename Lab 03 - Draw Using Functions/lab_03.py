65e8c81926c27d161bf9db7b88b3178f6d451fde
Rob Mixon <rmixon@enablecomp.com>
2023-02-01 16:48:48 -0700

    finished lab 3

--- Lab 03 - Draw Using Functions/lab_03.py
+++ Lab 03 - Draw Using Functions/lab_03.py
@@ -1,31 +1,45 @@
 import arcade
+import random
 from arcade.examples.sprite_explosion_particles import PARTICLE_FADE_RATE
 
-Screen_Width = 800
+Screen_Width = 1000
 Screen_Height = 800
-point_list = ((165, 495), (200, 760), (250, 700), (270, 650), (400, 670),
-                  (450, 780), (490, 620), (510, 730), (525, 700), (560, 630),
-                  (600, 760), (610, 690), (650, 650), (690, 730), (700, 700),
-                  (724, 665), (740, 615), (770, 720), (775, 705), (180, 770))
+star_coordinate_list = []
 
 def draw_light_from_house():
     arcade.draw_ellipse_filled(625, 320, 28, 48, arcade.color.DARK_YELLOW)
     arcade.draw_rectangle_filled(420, 320, 123, 48, arcade.color.DARK_YELLOW)
-    arcade.draw_rectangle_filled(420, 450, 58, 58, arcade.color.DARK_YELLOW)
-    arcade.draw_rectangle_filled(625, 450, 58, 58, arcade.color.DARK_YELLOW)
 
-def draw_upper_windows():
-    arcade.draw_rectangle_outline(420, 450, 60, 60, arcade.color.BLACK, 2)
-    arcade.draw_rectangle_outline(625, 450, 60, 60, arcade.color.BLACK, 2)
-    arcade.draw_line(390, 450, 450, 450, arcade.color.BLACK, 2)
-    arcade.draw_line(595, 450, 655, 450, arcade.color.BLACK, 2)
+def draw_upper_windows(x, y):
+    arcade.draw_rectangle_filled(x, y, 58, 58, arcade.color.DARK_YELLOW)
+    arcade.draw_rectangle_outline(x, y, 60, 60, arcade.color.BLACK, 2)
+    arcade.draw_line(x - 30, y, x + 30, y, arcade.color.BLACK, 2)
+    arcade.draw_line(x, y - 30, x, y + 30, arcade.color.BLACK, 2)
 
-def stars():
+def draw_house():
+    arcade.draw_lrtb_rectangle_filled(300, 700, 500, 250, arcade.color.JET)
+    arcade.draw_lrtb_rectangle_filled(600, 650, 625, 500, (20, 20, 20))
+    arcade.draw_triangle_filled(300, 500, 500, 600, 700, 500, (43, 43, 43))
+    arcade.draw_lrtb_rectangle_filled(600, 650, 350, 250, arcade.color.DARK_BROWN)
+    arcade.draw_circle_filled(610, 290, 3, arcade.color.BLACK)
+    arcade.draw_ellipse_outline(625, 320, 30, 50, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(420, 320, 125, 50, arcade.color.BLACK, 2)
+
+def stars(point_list):
     arcade.draw_points(point_list, arcade.color.GHOST_WHITE, 2)
 
 def moon():
     arcade.draw_circle_filled(100, 650, 50, arcade.color.GHOST_WHITE)
     arcade.draw_circle_filled(120, 650, 40, (27, 27, 27))
+
+def ground():
+    arcade.draw_lrtb_rectangle_filled(0, 999, 300, 0, (26, 36, 33))
+
+def tree(x, y):
+    arcade.draw_rectangle_filled(x, y, 30, 60, (69, 41, 19))
+    arcade.draw_triangle_filled(x - 30, y + 30, x, y + 70, x + 30, y + 30, arcade.color.DARK_GREEN)
+    arcade.draw_triangle_filled(x - 30, y + 60, x, y + 100, x + 30, y + 60, arcade.color.DARK_GREEN)
+    arcade.draw_triangle_filled(x - 30, y + 90, x, y + 130, x + 30, y + 90, arcade.color.DARK_GREEN)
 
 def main():
     arcade.open_window(Screen_Width, Screen_Height, "landscape")
@@ -36,42 +50,35 @@ def main():
     arcade.start_render()
 
     # Start Drawing
-    stars()
+    ground()
 
-    # Ground
-    arcade.draw_lrtb_rectangle_filled(0, 799, 300, 0, (26, 36, 33))
+    for x in range(0, 100):
+        x_stars = random.randint(0, 1000)
+        y_stars = random.randint(400, 800)
+        x_and_y_object = (x_stars, y_stars)
+        star_coordinate_list.append(x_and_y_object)
+
+    stars(star_coordinate_list)
 
     # House
-    arcade.draw_lrtb_rectangle_filled(300, 700, 500, 250, arcade.color.JET)
-
-    # Chimney
-    arcade.draw_lrtb_rectangle_filled(600, 650, 625, 500, (20, 20, 20))
-
-    # Roof
-    arcade.draw_triangle_filled(300, 500, 500, 600, 700, 500, (43, 43, 43))
-
-    # Door
-    arcade.draw_lrtb_rectangle_filled(600, 650, 350, 250, arcade.color.DARK_BROWN)
-
-    # Door Handle
-    arcade.draw_circle_filled(610, 290, 3, arcade.color.BLACK)
-
-    # Door Window
-    arcade.draw_ellipse_outline(625, 320, 30, 50, arcade.color.BLACK)
-
-    # Window at Ground Level
-    arcade.draw_rectangle_outline(420, 320, 125, 50, arcade.color.BLACK, 2)
+    draw_house()
 
     draw_light_from_house()
-    draw_upper_windows()
+    draw_upper_windows(390, 450)
+    draw_upper_windows(505, 450)
+    draw_upper_windows(620, 450)
     moon()
+
+    tree(50, 290)
+    tree(120, 290)
+    tree(850, 290)
+    tree(920, 290)
 
     # Finish drawing
     arcade.finish_render()
 
     # Keep the window up until someone closes it.
     arcade.run()
-
 
 # calling the main function
 main()
