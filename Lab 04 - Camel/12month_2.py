e840042ac3bc11e1b52e7f728076c8c1d03a6e07
RosikOsad <rosik911@gmail.com>
2023-03-06 16:04:36 +0200

    111

--- Lab 04 - Camel/12month_2.py
+++ Lab 04 - Camel/12month_2.py
@@ -2,4 +2,3 @@ months = "JanFebMarAprMayJunJulAugSepOctNovDec"
 n = int(input("Enter a month number: "))
 selected_month = [months[i:i+3] for i in range(0, len(months), 3)][n-1]
 print('selected month is: ', selected_month)
-5
